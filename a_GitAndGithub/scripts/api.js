1f4cdcbd5743971080171f3b6c832974f1420057
ISpectruM <ispectrumcode@gmail.com>
2017-06-14 13:20:36 +0300

    Cosmetic changes made.

--- a_GitAndGithub/scripts/api.js
+++ a_GitAndGithub/scripts/api.js
@@ -95,7 +95,7 @@
 
     // Loading of adverts
     $.mockjax(function (requestSettings) {
-        if (requestSettings.url==="https://mock.api.com/appdata/kid_rk/adverts" &&
+        if (requestSettings.url === "https://mock.api.com/appdata/kid_rk/adverts" &&
             requestSettings.method === "GET") {
             return {
                 response: function (origSettings) {
